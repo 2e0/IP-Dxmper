a74559aca19a11e0d83f69f37f36ae5c22c871e7
Naff Ruby <110056919+2e0@users.noreply.github.com>
2022-07-28 23:05:10 +0200

    Add files via upload

---
+++ cpaninstall.py
@@ -0,0 +1,12 @@
+import os
+
+#bref comme tu verifies le code source hein, sa va prendrprendre du temps tqt c'est cpan desolé, encore ce truc je voulais le faire en raku donc t'imagines genre l'installation palalalpalalala  
+
+os.system('cpan HTTP::Client')
+os.system('cpan Net::IP')
+os.system('cpan Color::Fade')
+os.system('cpan WWW::Pastebin::Sprunge::Create')
+
+
+
+
